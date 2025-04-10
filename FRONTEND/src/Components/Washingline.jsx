853471aa2ccef81ed63098920e047fbaa39b5148
SriNarula <166502791+SriNarula@users.noreply.github.com>
2025-04-11 00:49:22 +0530

    Update Washingline.jsx

--- FRONTEND/src/Components/Washingline.jsx
+++ FRONTEND/src/Components/Washingline.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom"; 
 import railwayWashing from "../assets/Washingline.jpeg";
-import "./WashingLine.css";
+import "./Washingline.css";
 
 const WashingLine = () => {
   const navigate = useNavigate(); 
