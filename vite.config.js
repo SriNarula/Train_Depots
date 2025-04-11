1efb1758169c87977dc19d39737035b1143aef03
SriNarula <166502791+SriNarula@users.noreply.github.com>
2025-04-11 20:42:07 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,15 @@
-export default {
+import { defineConfig } from 'vite';
+
+export default defineConfig({
   server: {
     host: '0.0.0.0',
-    port: parseInt(process.env.PORT) || 4173
+    port: parseInt(process.env.PORT) || 4173,
   },
   preview: {
     host: '0.0.0.0',
-    port: parseInt(process.env.PORT) || 4173
+    port: parseInt(process.env.PORT) || 4173,
+  },
+  define: {
+    'process.env': process.env,
   }
-}
+});
